f97b47b0bc821c7f2c8a4092ebe1627f9436c14d
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-04-02 23:23:10 -0300

    Comportamentos das classes

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 class Cliente {
     nome;
     cpf;
+}
+class ContaCorrente {
     agencia;
     saldo;
-    rg;
+    
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Henrique";
 cliente1.cpf = 14562578796;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Cláudia";
 cliente2.cpf = 57864578112;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteHenrique = new ContaCorrente();
+contaCorrenteHenrique.saldo = 0;
+contaCorrenteHenrique.agencia = 1001;
+
+
+console.log(contaCorrenteHenrique.saldo);
+contaCorrenteHenrique.saldo = 100;
+console.log(contaCorrenteHenrique.saldo);
+contaCorrenteHenrique.sacar(50);
+
+
+console.log(contaCorrenteHenrique.saldo);
+console.log(cliente1);
+console.log(cliente2);
