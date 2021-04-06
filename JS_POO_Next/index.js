4d70e15d9eb7ca6f912759febdac34ea429ca871
Henrique Madureira de Jesus <henrique.jesus@concert.com.br>
2021-04-05 21:19:04 -0300

    Classe abstrata

--- JS_POO_Next/index.js
+++ JS_POO_Next/index.js
@@ -1,15 +1,12 @@
 import {Cliente} from "./Cliente.js"
+import { Conta } from "./conta.js";
 import {ContaCorrente} from "./ContaCorrente.js"
 import { ContaPoupanca } from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente("Henrique", 14562578796);
 
 const contaCorrenteHenrique = new ContaCorrente(1001, cliente1, 1001);
-contaCorrenteHenrique.depositar(500);
-contaCorrenteHenrique.sacar(100);
-
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-contaPoupanca._sacar(10);
+const conta = new Conta(0, cliente1, 1001);
 
-console.log(contaPoupanca);
-console.log(contaCorrenteHenrique);
+console.log(conta);
